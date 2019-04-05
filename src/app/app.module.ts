5641bbdac2f476cb7f21f701436cc8da73ab1085
Nayomi Morita <nayomi.morita@gmail.com>
2019-04-05 13:37:16 -0700

    I have api-keys in my gitignore but it shows in my github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-
-import { AppComponent } from './app.component';
-import { ListComponent } from './list/list.component';
-=======
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
@@ -14,16 +7,6 @@ import { ListComponent } from './list/list.component';
 import { masterFirebaseConfig } from './api-keys';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
->>>>>>> 9eaa245... add further setup of app.module.ts
-=======
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { AppComponent } from './app.component';
-import { ListComponent } from './list/list.component';
-import { masterFirebaseConfig } from './api-keys';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
->>>>>>> 45045a10739d9038de357b89454517945ba2f431
 
 export const firebaseConfig = {
   apiKey: masterFirebaseConfig.apiKey,
