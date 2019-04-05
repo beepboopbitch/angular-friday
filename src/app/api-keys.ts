526802da70e0e3e129c832d9105a1fb46e180ba8
Nayomi Morita <nayomi.morita@gmail.com>
2019-04-05 13:01:29 -0700

    add firbase api-key

---
+++ src/app/api-keys.ts
@@ -0,0 +1,8 @@
+export const masterFirebaseConfig = {
+  apiKey: "AIzaSyDncT5Y-AdWi8uJIdVRV5ywYsgpsXPZJe0",
+  authDomain: "angular-friday.firebaseapp.com",
+  databaseURL: "https://angular-friday.firebaseio.com",
+  projectId: "angular-friday",
+  storageBucket: "angular-friday.appspot.com",
+  messagingSenderId: "729153360057"
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
+import { masterFirebaseConfig } from './api-keys';
 
 
 @NgModule({
